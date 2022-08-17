24ff4e9fa27930c4f5f71825e803397471d07d3b
ShivalikSharma98 <Shivaliksharma98@gmail.com>
2022-08-16 22:24:37 -0600

    object types

--- app.ts
+++ app.ts
@@ -1,3 +1,4 @@
+// TYPES
 function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     const result = n1 + n2;
     if (showResult) {
@@ -6,11 +7,25 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
         return result;
     }
 }
-
+// EXAMPLE OF TYPESCRIPT INFERENCE TYPE, IT UNDERSTANDS NUMBER2 US A NUMBER AND PRINTRESULT IS A BOOLEAN WITHOUT IT BEING DECLARED AS SUCH LIKE WE DID FOR NUMBER1
 let number1: number;
 number1 = 5;
+
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = 'Result is: ';
 
 const result = add(number1, number2, printResult, resultPhrase);
+
+// OBJECT TYPES
+let person: {
+    name: string;
+    age: number;
+}
+
+person = {
+    name: 'Shiv',
+    age: 24,
+}
+
+console.log(person.name);
